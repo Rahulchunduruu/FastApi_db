91b1c51559a717c792cde0d8a073d834de0d2271
Rahulchunduruu <rahulchunduruu@gmail.com>
2026-06-03 15:21:47 +0530

    adding streamlit style in config files

--- config.py
+++ config.py
@@ -4,18 +4,17 @@ import streamlit as st
 
 load_dotenv()
 
+
 class Config:
-    #APP_NAME = os.getenv('APP_NAME')
-    #DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    #DATABASE_URL = os.getenv('DATABASE_URL')
-    #API_KEY = os.getenv('API_KEY')
-    #EXTERNAL_API_URL = os.getenv('EXTERNAL_API_URL')
-    DATABASE_URL=st.secrets("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL") or st.secrets.get("DATABASE_URL")
+    API_KEY = os.getenv("API_KEY") or st.secrets.get("API_KEY")
+    EXTERNAL_API_URL = os.getenv("EXTERNAL_API_URL") or st.secrets.get("EXTERNAL_API_URL")
+
     if not DATABASE_URL:
-        raise ValueError("DATABASE_URL not found in .env file. Please add your database URL.")
-    API_KEY=st.secrets("API_KEY")
+        raise ValueError("DATABASE_URL not found")
+
     if not API_KEY:
-        raise ValueError("API_KEY not found in .env file. Please add your API key.")
-    EXTERNAL_API_URL=st.secrets("EXTERNAL_API_URL")
+        raise ValueError("API_KEY not found")
+
     if not EXTERNAL_API_URL:
-        raise ValueError("EXTERNAL_API_URL not found in .env file. Please add your API URL.")
+        raise ValueError("EXTERNAL_API_URL not found")
